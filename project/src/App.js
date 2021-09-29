fc8a9d28a5ec5d17e7b4e40290d9e30323b943cf
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-09-28 23:22:43 -0300

    Trabalhando com props

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Davi';
+
+  const nome = "Ellen";
 
   return (
     <div className="App">
-      <h1>Olá Mundo! Olá React!</h1>
-      <p>Esta é minha primeira página web com React!</p>
-      <p>Olá, {name} </p>
-      <HelloWorld />
+      <SayMyName nome="Davi" />
+      <SayMyName nome="Aline" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Davi" idade="23" profissao="Desenvolvedor Web" foto="https://avatars.githubusercontent.com/u/35413910?v=4"/>
     </div>
   );
 }
