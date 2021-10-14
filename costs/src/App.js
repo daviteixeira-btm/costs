cc7eecc6869656170552690ed0a11a6e7f9b2a07
Davi Teixeira <daviteixeira.btm@gmail.com>
2021-10-14 11:11:33 -0300

    Página para o formulário de projeto

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
